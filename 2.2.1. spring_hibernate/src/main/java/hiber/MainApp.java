5d2fd57ab837df1aa10ce603b60529b117dfd10d
Kudashev-020 <kudashev.020@gmail.com>
2023-04-07 23:07:25 +0300

    add method getUserByCar

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -22,7 +22,7 @@ public class MainApp {
 
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+ user.getId());
+         System.out.println("Id = " + user.getId());
          System.out.println("First Name = " + user.getFirstName());
          System.out.println("Last Name = " + user.getLastName());
          System.out.println("Email = " + user.getEmail());
@@ -30,6 +30,12 @@ public class MainApp {
          System.out.println();
       }
 
-      context.close();
+      User user = userService.getUserByCar("BMW", 1);
+
+      System.out.println("Id = " + user.getId() + "\n" +
+                         "First Name = " + user.getFirstName() + "\n" +
+                         "Last Name = " + user.getLastName() + "\n" +
+                         "Email = " + user.getEmail() + "\n" +
+                         "Car = " + user.getCar());
    }
 }
